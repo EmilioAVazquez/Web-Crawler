67e7ea1885cff965a0e4dff16281cca45520f4de
EmilioAVazquez <emibars97@gmail.com>
2018-07-20 21:19:24 -0500

    Chnages to crawler and added input file.

--- spidy.py
+++ spidy.py
@@ -1,19 +1,10 @@
 from selenium import webdriver
-#This library, Selenium, connects Python with a web browser in your machine, a.k.a. Chrome, Firefox, etc.
 from selenium.webdriver.common.by import By
-#This lbrary is use to wait for your web browser to wait to load a page or element in the page.
 from selenium.webdriver.chrome.options import Options
-#This library is use to make changes to the webbrowser, like adding an Ad block or specifying a path.
 import xml.etree.cElementTree as ET
-#This library is use to make an xml tree that will store the data you scrape.
 import multiprocessing
-#This library will increment the processeing and power of the webcrawler
-import numpy as np
-#This librbary will keep a log of some data variables.
 import time
-#This lobrary keep track of the running speed of the code
 
-#Crate or import a data type of your choice to facilitate the network transversal (I recomend a stack or queue data type)
 class Stack_Words(object):
     """
     The invariant of this data structure is that a word cannot be entered more than once.
@@ -121,27 +112,33 @@ class Stack_Words(object):
         file.close()
 
 #Global variables
-path_to_extension   = r'/Users/emiliovazquez/Desktop/3.31.2_0'#This is the direction to the Adblocker on my computer
-path_to_driver1     =  "/Users/emiliovazquez/Desktop/govMex/chromedriver1"#This is the direction of one webdriver
-path_to_driver2     =  "/Users/emiliovazquez/Desktop/govMex/chromedriver2"#This is the direction of another webdriver
-# instantiate a chrome options object so you can set the size, extenssions, and headless preferences
+path_to_extension   = r'/home/emiliovazquez/Web-Crawler/Webdriver/3.31.2_0'
+path_to_driver1     =  "/home/emiliovazquez/Web-Crawler/Webdriver/chromedriver1"
+path_to_driver2     =  "/home/emiliovazquez/Web-Crawler/Webdriver/chromedriver2"
+path_to_driver3     =  "/home/emiliovazquez/Web-Crawler/Webdriver/chromedriver3"
+path_to_driver4     =  "/home/emiliovazquez/Web-Crawler/Webdriver/chromedriver4"
+# instantiate a chrome options object so you can set the size and headless preference
 chrome_options      = Options()
 chrome_options.add_argument('load-extension=' + path_to_extension)#Adblocker
 chrome_options.add_argument("--headless")#Headless
-chrome_options.add_argument("--window-size=1920x1080")#size
-#Initializations of web browsers
+chrome_options.add_argument("--window-size=1920x1080")
 browser1            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver1 , port=9515)
 browser2            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver2 , port=9514)
+browser3            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver3 , port=9513)
+browser4            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver4 , port=9512)
 
-#This is the function that will do most of the job, here webpage will be tested step by step
 def job(browser_number, word, queue):
 
     if   browser_number == 1:
         browser =  browser1
     elif browser_number  == 2:
         browser =  browser2
+    elif browser_number  == 3:
+        browser =  browser3
+    elif browser_number  == 4:
+        browser =  browser4
 
-    url      = ""#some URL
+    url      = "http://www.elmundo.es/diccionarios/"#URL to attack
     word = input_words.get()[0]
     #print (input_words.get()[0].encode('ascii', 'ignore'))#print(input_words.get()[0].encode('latin1').decode('utf8'))
     #print (word.encode('ascii', 'ignore'))#print(word.encode('latin1').decode('utf8'))
@@ -196,56 +193,44 @@ def update_synonyms_Tree(queue, xml_tree):
 def reset_browsers():
     global browser1
     global browser2
+    global browser3
+    global browser4
 
     browser1.quit()
     browser2.quit()
+    browser3.quit()
+    browser4.quit()
 
     browser1            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver1 , port=9515)
     browser2            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver2 , port=9514)
-
-def plot_resize (pw, t, y, const_size, temp_t, number_browsers):
-    size = t.shape[0] - const_size
-
-    if size <= 0 :
-        pw.setXRange(t[0], t[t.shape[0]-1], padding=0)
-        pw.setYRange(np.amin(y), np.amax(y), padding=0)
-        pw.plot(t, y, clear=True)
-
-    else :
-        pw.setXRange( t[size] , t[size + const_size - 1], padding=0)
-        pw.setYRange(np.amin(np.amin(y[size:])), np.amax(y[size:]), padding=0)
-        pw.plot(t[size:], y[size:], clear=True)
+    browser3            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver3 , port=9513)
+    browser4            = webdriver.Chrome(chrome_options=chrome_options, executable_path=path_to_driver4 , port=9512)
 
 if __name__ == '__main__':
     #Data paths: input(words), output(xml file path and xml tree)
-    path_to_words       = "/Users/emiliovazquez/Desktop/word_space/listado-general.txt"
-    path_to_notlogged   = "/Users/emiliovazquez/Desktop/word_space/notlogged.txt"
-    path_to_logged      = "/Users/emiliovazquez/Desktop/word_space/logged.txt"
-    path_to_backupnl    = "/Users/emiliovazquez/Desktop/word_space/notlogged1.txt"
-    path_to_backupl     = "/Users/emiliovazquez/Desktop/word_space/logged1.txt"
-    path_to_seeds       = "/Users/emiliovazquez/Desktop/seeds.txt"
-    path_to_synonymsDB  = "/Users/emiliovazquez/Desktop/word_space/database/synWords1.xml"
+    path_to_words       = "/home/emiliovazquez/Web-Crawler/Input_Files/listado-general.txt"
+    path_to_notlogged   = "/home/emiliovazquez/Web-Crawler/Input_Files/notlogged.txt"
+    path_to_logged      = "/home/emiliovazquez/Web-Crawler/Input_Files/logged.txt"
+    path_to_backupnl    = "/home/emiliovazquez/Web-Crawler/Input_Files/notlogged1.txt"
+    path_to_backupl     = "/home/emiliovazquez/Web-Crawler/Input_Files/logged1.txt"
+    path_to_seeds       = "/home/emiliovazquez/Web-Crawler/Input_Files/seeds.txt"
+    path_to_synonymsDB  = "/home/emiliovazquez/Web-Crawler/Input_Files/synWords1.xml"
     synonyms_xmlTree    = ET.Element("data")
 
     #User update interface
     flag = True
     downloaded = 0
-    const_size = 40#window x-axis size
-    t = np.array([])#x-axis data
-    y = np.array([])#y-axis data
-    pw = pg.plot()#plots the seconds per word to download
     ini_time = time.time()
-    t = np.append(t, [ini_time])
-    y = np.append(y, [0])
+
 
     input_words = Stack_Words([])
     input_words.load_words(path_to_notlogged)
-    input_words.load_words(path_to_logged)
+    input_words.load_logged(path_to_logged)
     input_words.push(retrive_fromfile_array(path_to_seeds))
     print(input_words.get()[0].encode('ascii','ignore'))
     queue = multiprocessing.Queue()#queue used for communication among the browsers
-    number_browsers = 2#may chaneg dependingon the number of parallel browsers
-    browsers = [1,2]
+    number_browsers = 4#may chaneg dependingon the number of parallel browsers
+    browsers = [1,2,3,4]
 
     int_time = time.time()
 
@@ -260,18 +245,15 @@ if __name__ == '__main__':
         input_words.push(update_synonyms_Tree(queue, synonyms_xmlTree))
 
         downloaded = downloaded + number_browsers
-        temp_t = time.time() - ini_time
-        y = np.append(y, [(temp_t - t[t.shape[0]-1])/number_browsers])
-        t = np.append(t, [temp_t])
-        plot_resize(pw, t, y, const_size, temp_t, number_browsers)
-        pg.QtGui.QApplication.processEvents()
 
-        if downloaded%(30) == 0:
+        if downloaded%(24) == 0:
             print(downloaded)
-            break#the number of iterations may be as big as you want
+            break
 
-        if downloaded%(800) == 0:
-            print(downloaded)
+        if downloaded%(8) == 0:
+            print('Number of words dondownloaded: ', downloaded,' Speed: ', (time.time() - ini_time)/downloaded, 'sec/wrd', ' Last words len ', input_words.get(0)[0])
+
+        if downloaded%(1400) == 0:
             reset_browsers()
 
 
@@ -280,6 +262,8 @@ if __name__ == '__main__':
     tree.write(path_to_synonymsDB)
     browser1.quit()
     browser2.quit()
+    browser3.quit()
+    browser4.quit()
     #print(new)
     #synWords.close()
     print("Finished!")
